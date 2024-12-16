00fa4b12e9760e5df7430112295ace6d9bdd4263
Kboti2005 <93659455+Kboti2005@users.noreply.github.com>
2024-12-16 15:18:55 +0100

    .

---
+++ Kalapacsvetes/Sportolo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapacsvetes
+{
+    internal class Sportolo
+    {
+        public Sportolo() 
+        {
+            int helyezes = 0;
+            double eredmeny = 0;
+            string nev = "";
+            int orszagKod = 0;
+            string helyszin = "";
+            int datum = 0;
+        }
+    }
+}
